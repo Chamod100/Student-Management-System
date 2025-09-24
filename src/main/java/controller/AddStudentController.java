bfd4154fe113d0e907c72e9c5bd1b322639ea000
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-09-24 18:36:37 +0530

    write the Button Action

--- src/main/java/controller/AddStudentController.java
+++ src/main/java/controller/AddStudentController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class AddStudentController {
+    public void handleAddStudent(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
 }
