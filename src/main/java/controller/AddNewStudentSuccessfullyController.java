a4116820745c0c2ee6f91826af64d76cec5e516e
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-09-25 22:44:22 +0530

    create AddNewStudentSuccessfullyController with event handling methods

---
+++ src/main/java/controller/AddNewStudentSuccessfullyController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.event.ActionEvent;
+
+public class AddNewStudentSuccessfullyController {
+    public void handleBackToHome(ActionEvent actionEvent) {
+    }
+
+    public void handleAddNewStudent(ActionEvent actionEvent) {
+    }
+}
